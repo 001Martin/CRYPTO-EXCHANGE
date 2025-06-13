818fb809d5c18f69fd2ba9680df03f8aaa7316d6
001Martin <luterking65@gmail.com>
2025-06-13 11:59:06 +0100

    fix name correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 function Home() {
   return (
     <div className="flex justify-center items-center py-20 px-4 flex-col">
-      <h1 className="text-3xl lg: text-8xl font-bold font-mono">
+      <h1 className="text-3xl lg:text-8xl font-bold font-mono">
         {" "}
-        <span className="text-blue-800 animate-pulse">24/7</span> Luterking
-        Crypto trade
+        <span className="text-blue-300 animate-pulse"></span> Kings Crypto trade
       </h1>
       <p className="text-lg lg:text-2xl mt-4 text-gray-400 font-serif">
         Get access to all your favorite crypto currencies in one place
